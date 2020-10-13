588951b73c5df6a0796f86a12ad98d858bc702d9
unknown <administrator@TRANSAD.LOCAL>
2020-10-13 18:09:31 -0400

    working login/logout

--- dbtest.js
+++ dbtest.js
@@ -1,24 +1,19 @@
-const express = require('express')
-const https = require('https');
-const app = express()
+
+const express = require('express');
+const app = express();
 var session = require('express-session'); 
-const createUser = require('./createUser');
-var fs = require('fs');
 const bcrypt = require('bcrypt');
+var http = require('http');
+var fs = require('fs');
 var mysql = require('mysql');
-app.use(express.static(__dirname + '/website'));
 
 app.use(session({ secret: 'happy jungle', 
                   resave: false, 
                   saveUninitialized: false, 
                   cookie: { maxAge: 6000000 }}));
 
-
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-
-
-process.env.PORT = 3000;
-process.env.IP = '10.0.2.6';
+process.env.PORT = 3001;
+process.env.IP = '127.0.0.1';
 
 const conInfo = 
 {
@@ -28,15 +23,7 @@ const conInfo =
     database: "webauth"
 };
 
-
-
-app.all('/', (req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var index = fs.readFileSync('website/index.html');
-  res.end(index);
-})
-
-
+app.all('/',(req,res)=>{res.send('page loaded');}) //Default 
 app.all('/register',register);//Creates a new User
 app.all('/whoIsLoggedIn',whoIsLoggedIn)//Checks who is logged in
 app.all('/login',login);//logs user in
@@ -48,11 +35,17 @@ app.all('/testQ',testquery);//TEST
 app.all('/testsession',testsession);//TEST
 app.all('/viewsession',viewsession);//TEST
 
+
+
+app.all('/',(req,res)=>{
+    res.send('page loaded');
+})
+
 app.listen(process.env.PORT,  process.env.IP, startHandler())
 
 function startHandler()
 {
-  console.log(`Server listening at http://${process.env.IP}:${process.env.PORT}; started at ${time()}`);
+  console.log('Server listening on port ' + process.env.PORT)
 }
 
 function writeResult(req, res, obj)
@@ -61,106 +54,6 @@ function writeResult(req, res, obj)
   res.write(JSON.stringify(obj));
   res.end('');
 }
-
-//===================================================
-//AD/VPN WRAPPER FUNCTIONS
-app.get('/isalive', (req, res) => {
-  res.send("Yes the Server is Alive.")
-})
-
-app.get('/createaccount', (req, res) => {
-    if (IsLoggedIn(req,res))
-    {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      var index = fs.readFileSync('website/createaccount.html');
-      res.end(index);
-    }
-    else
-    {
-      res.redirect("http://10.0.2.6:3000/login.html");
-    }
-})
-
-app.get('/newuser', (req, res) => {
-  
-  //res.send('NEW USER REQUEST RECIEVED');
-  console.log("/newuser has been pinged with the following data:")
-  console.log(req.query);  
-  
-  createUser.psNewUser(req.query);
-
-  res.send(`Request Received. Sent New User Request to the server`);
-  
- 
-  
-})
-
-app.get('/vpn',(req,res)=>{
-
-console.log("API REQUEST RECIEVED");
-
-console.log(req.query.q);
-
-    const data = req.query.q;
-
-    const options = {
-    hostname: 'transad.inertialframe.net',
-    port: 5556,
-    path: '/api',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': data.length,
-        'X-VPNADMIN-HUBNAME':'administrator',	
-        'X-VPNADMIN-PASSWORD':'420Blazeit!'
-    }
-  }
-
-  const POST = https.request(options, response => {
-    console.log(`statusCode: ${response.statusCode}`)
-    response.on('data', d => {
-        const JSONRes = JSON.parse(d);
-        console.log(JSONRes);
-        res.send(JSONRes);
-    })
-  })
-  POST.on('error', error => {
-    console.error(error)
-  })
-  POST.write(data)
-  POST.end()
-})
-
-function time(){
-  let date_ob = new Date();
-
-// current date
-// adjust 0 before single digit date
-let date = ("0" + date_ob.getDate()).slice(-2);
-
-// current month
-let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-
-// current year
-let year = date_ob.getFullYear();
-
-// current hours
-let hours = date_ob.getHours();
-
-// current minutes
-let minutes = date_ob.getMinutes();
-
-// current seconds
-let seconds = date_ob.getSeconds();
-
-// prints date in YYYY-MM-DD format
-//console.log(year + "-" + month + "-" + date);
-
-// prints date & time in YYYY-MM-DD HH:MM:SS format
-return year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
-
-}
-// END OF AD/VPN WRAPPER FUNCTIONS
 //===================================================
 //TEST FUNCTIONS
 function testdbconnection(req,res)
@@ -243,14 +136,6 @@ function whoIsLoggedIn(req, res)
     writeResult(req, res, {'error' : 'Nobody is logged in.'});
   else
     writeResult(req, res, req.session.user);
-}
-
-function IsLoggedIn(req, res)
-{
-  if (req.session.user == undefined)
-    return false;
-  else
-    return true;
 }
 //END OF HELPERFUNCTIONS
 //=============================================================
@@ -354,13 +239,6 @@ function logout(req, res)
 {
   req.session.user = undefined;
   writeResult(req, res, {'error' : 'Nobody is logged in.'});
-}
-function whoIsLoggedIn(req, res)
-{
-  if (req.session.user == undefined)
-    writeResult(req, res, {'error' : 'Nobody is logged in.'});
-  else
-    writeResult(req, res, req.session.user);
 }
 //END OF DBFUNTIONS
 
